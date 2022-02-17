51e03406788aa51cda23e0f07561f752eb04f452
Guido <ppapaguido@gmail.com>
2022-02-17 17:45:18 +0100

    refactoring index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,27 +8,42 @@
         <div>
             <a href="{{route("posts.create")}}"><button type="button" class="btn btn-outline-secondary">Crea post</button></a>
         </div>
+
         <div class="col-md-12">
-            @foreach ($posts as $post)
-            <div class="card mt-3">
-                <div class="card-header">{{$post->title}}</div>
-                <div class="card-body">
-                    <p>
-                        {{$post->content}} 
-                    </p>
-                    <div class="pubblished">pubblicato il: {{$post->created_at}}</div>
-                    <a href="{{route("posts.show", $post->id)}}"><button type="button" class="btn btn-outline-primary">Vai al post</button></a>
-                    <a href="{{route("posts.edit", $post->id)}}"><button type="button" class="btn btn-outline-warning">Modifica il post</button></a>
-                    <form action="{{route("posts.destroy", $post->id)}}" method="POST">
+
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">azioni</th>
+                  </tr>
+                </thead>
+                <tbody>
+                @foreach ($posts as $post)
+                <tr>
+                    <th scope="row">{{$post->id}}</th>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->slug}}</td>
+                    <td>
+                        <a href="{{route("posts.show", $post->id)}}"><button type="button" class="btn btn-outline-primary w-100">Vai al post</button></a>
+                        <a href="{{route("posts.edit", $post->id)}}"><button type="button" class="btn btn-outline-warning w-100">Modifica il post</button></a>
+                        <form action="{{route("posts.destroy", $post->id)}}" method="POST">
                         @csrf
                         @method("DELETE")
-                        <button type="submit" class="btn btn-outline-danger">Cancella</button>
-                      </form>
-                </div>
-            </div>
-            @endforeach
+                            <button type="submit" class="btn btn-outline-danger w-100">Cancella</button>
+                        </form>
+                    </td>
+                  </tr> 
+                @endforeach
+                </tbody>
+              </table>
+
 
         </div>
     </div>
 </div>
 @endsection
+
+
